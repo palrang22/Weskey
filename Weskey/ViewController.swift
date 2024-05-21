d398923cb4f1494b79399b38ab903e960bd9f846
kseunghui@gmail.com <kseunghui@gmail.com>
2024-05-21 16:42:48 +0900

    Initial Commit

---
+++ Weskey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weskey
+//
+//  Created by 김승희 on 5/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
