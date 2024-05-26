9ae515f71597a496ca3969a6ebda2e3b76f9cf6d
kseunghui@gmail.com <kseunghui@gmail.com>
2024-05-26 14:17:32 +0900

    Initial Commit

---
+++ Weskey/Community/CommunityModel.swift
@@ -0,0 +1,8 @@
+//
+//  CommunityModel.swift
+//  Weskey
+//
+//  Created by 김승희 on 5/26/24.
+//
+
+import Foundation
---
+++ Weskey/Community/SamplePosts.swift
@@ -0,0 +1,8 @@
+//
+//  SamplePosts.swift
+//  Weskey
+//
+//  Created by 김승희 on 5/26/24.
+//
+
+import Foundation
