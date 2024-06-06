862c1b83b39595b4c1a0cb6dc4d8e9bda5f60ae8
kseunghui@gmail.com <kseunghui@gmail.com>
2024-06-06 19:14:58 +0900

    [fix] label width 작을 때 말풍선 사라지는 버그 수정

--- Weskey/ChatBot/ChatMessageCellCollectionViewCell.swift
+++ Weskey/ChatBot/ChatMessageCellCollectionViewCell.swift
@@ -10,54 +10,57 @@ class ChatMessageCell: UICollectionViewCell {
     @IBOutlet weak var trailingConstraint: NSLayoutConstraint!
     
     func configure(with message: Message) {
-        messageLabel.text = message.text
-        messageLabel.numberOfLines = 0
-        messageLabel.sizeToFit()
-        
-        let maxMessageWidth = UIScreen.main.bounds.width - 20
-        let messageTextWidth = messageLabel.sizeThatFits(CGSize(width: maxMessageWidth, height: CGFloat.greatestFiniteMagnitude)).width
-        let constrainedWidth = min(maxMessageWidth, messageTextWidth)
-        
-        if message.isUser {
-            chatBotImageView.isHidden = true
-            chatBotName.isHidden = true
-            leadingConstraint.constant = max(30, UIScreen.main.bounds.width - constrainedWidth)
-            trailingConstraint.constant = 10
-        } else {
-            chatBotImageView.isHidden = false
-            chatBotName.isHidden = false
-            chatBotImageView.image = UIImage(named: "chatbotCharacterChatting")
-            chatBotName.text = "We's Key"
-            leadingConstraint.constant = 10
-            trailingConstraint.constant = max(50, UIScreen.main.bounds.width - constrainedWidth - 10)
+            messageLabel.text = message.text
+            messageLabel.numberOfLines = 0
+            messageLabel.sizeToFit()
+            
+            let maxMessageWidth = UIScreen.main.bounds.width - 20
+            let messageTextWidth = messageLabel.sizeThatFits(CGSize(width: maxMessageWidth, height: CGFloat.greatestFiniteMagnitude)).width
+            let constrainedWidth = min(maxMessageWidth, messageTextWidth)
+            
+            let fixedWidth: CGFloat = 150
+            let finalWidth = (messageTextWidth <= fixedWidth) ? fixedWidth : constrainedWidth
+            
+            if message.isUser {
+                chatBotImageView.isHidden = true
+                chatBotName.isHidden = true
+                leadingConstraint.constant = max(30, UIScreen.main.bounds.width - finalWidth)
+                trailingConstraint.constant = 10
+            } else {
+                chatBotImageView.isHidden = false
+                chatBotName.isHidden = false
+                chatBotImageView.image = UIImage(named: "chatbotCharacterChatting")
+                chatBotName.text = "We's Key"
+                leadingConstraint.constant = 10
+                trailingConstraint.constant = max(50, UIScreen.main.bounds.width - finalWidth - 10)
+            }
+            
+            let userColor = UIColor(hex: "#632810")
+            messageBackgroundView.backgroundColor = message.isUser ? UIColor.white : userColor
+            messageLabel.textColor = message.isUser ? UIColor.black : UIColor.white
+            messageLabel.textAlignment = message.isUser ? .right : .left
+            
+            messageBackgroundView.layer.cornerRadius = 10
+            messageBackgroundView.layer.borderWidth = 1.0
+            messageBackgroundView.layer.borderColor = userColor.cgColor
+            
+            layoutIfNeeded()
         }
-        
-        let userColor = UIColor(hex: "#632810")
-        messageBackgroundView.backgroundColor = message.isUser ? UIColor.white : userColor
-        messageLabel.textColor = message.isUser ? UIColor.black : UIColor.white
-        messageLabel.textAlignment = message.isUser ? .right : .left
-        
-        messageBackgroundView.layer.cornerRadius = 10
-        messageBackgroundView.layer.borderWidth = 1.0
-        messageBackgroundView.layer.borderColor = userColor.cgColor
-        
-        layoutIfNeeded()
     }
-}
-    
-extension UIColor {
-    convenience init(hex: String) {
-        var hexString: String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
-        if hexString.hasPrefix("#") {
-            hexString.remove(at: hexString.startIndex)
+
+    extension UIColor {
+        convenience init(hex: String) {
+            var hexString: String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+            if hexString.hasPrefix("#") {
+                hexString.remove(at: hexString.startIndex)
+            }
+            var rgbValue: UInt64 = 0
+            Scanner(string: hexString).scanHexInt64(&rgbValue)
+            self.init(
+                red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                alpha: CGFloat(1.0)
+            )
         }
-        var rgbValue: UInt64 = 0
-        Scanner(string: hexString).scanHexInt64(&rgbValue)
-        self.init(
-            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-            alpha: CGFloat(1.0)
-        )
     }
-}
